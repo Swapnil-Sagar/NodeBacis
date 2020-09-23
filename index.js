48afa0dfcacde7fefd80bf103485441ab41aae57
Swapnil-Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-09-23 20:35:25 +0530

    Created a callback function

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var rect = require('./rectangle');
+var rect = require("./rectangle");
 
-function solveRect(l,b) {
-    console.log("Solving for Rectangle with l = " + l + " and b = " + b);
+function solveRect(l, b) {
+  console.log("Solving for Rectangle with l = " + l + " and b = " + b);
 
-    if(l <=0 || b <= 0){
-        console.log("Dimentions should be greater than 0");
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR: ", err.message);
+    } else {
+      console.log("The area of rectangle is " + rectangle.area());
+      console.log("The perimeter of rectangle is " + rectangle.perimeter());
     }
-    else{
-        console.log("The area of rectangle is " + rect.area(l,b));
-        console.log("The perimeter of rectangle is " + rect.perimeter(l,b));
-        
-    }
+  });
+  console.log("Already called rect");
 }
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
